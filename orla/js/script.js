b8466b0927964eb0286176d88a5126dfa27be74e
SrHunter0 <135316078+SrHunter0@users.noreply.github.com>
2023-06-05 18:47:14 -0300

    Add files via upload

---
+++ orla/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('.cardápios').slick({
+      dots: true,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 3,
+      adaptiveHeight: true
+    });
+  });
